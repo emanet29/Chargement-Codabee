d9d3c0fdedbd85a4c7180cfd752b7a05cce703c5
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-08-21 14:01:27 +0200

    alveoles + logo

--- Chargement_Codabee/ViewController.swift
+++ Chargement_Codabee/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var alveoles = [UIImageView]()
+    var logo: UIImageView!
+    var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ajouterAlveole(valeurX: 0.5, valeurY: 0.5, image: #imageLiteral(resourceName: "honeyComb_empty"))
+        ajouterAlveole(valeurX: 0.5, valeurY: 1.5, image: #imageLiteral(resourceName: "honeyComb_empty"))
+        ajouterAlveole(valeurX: 0.5, valeurY: 2.5, image: #imageLiteral(resourceName: "honeyComb_empty"))
+        ajouterAlveole(valeurX: 1.25, valeurY: 1, image: #imageLiteral(resourceName: "honeyComb_empty"))
+        ajouterAlveole(valeurX: 1.25, valeurY: 2, image: #imageLiteral(resourceName: "honeyComb_empty"))
+        ajouterAlveole(valeurX: 2, valeurY: 0.5, image: #imageLiteral(resourceName: "honeyComb_empty"))
+        ajouterAlveole(valeurX: 2, valeurY: 1.5, image: #imageLiteral(resourceName: "honeyComb_bee"))
+        ajouterAlveole(valeurX: 2.75, valeurY: 1, image: #imageLiteral(resourceName: "honeyComb_empty"))
+        ajouterAlveole(valeurX: 3.5, valeurY: 0.5, image: #imageLiteral(resourceName: "honeyComb_empty"))
+        
+        logo = UIImageView(frame: CGRect(x: 0, y: 50, width: view.frame.width, height: 150))
+        logo.contentMode = .scaleAspectFit
+        logo.image = #imageLiteral(resourceName: "comb_logo")
+        view.addSubview(logo)
+        
+        label = UILabel(frame: CGRect(x: 100, y: 200, width: view.frame.width - 200, height: 50))
+        label.text = "Chargement"
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.adjustsFontSizeToFitWidth = true
+        label.textColor = UIColor(red: 255 / 255, green: 204 / 255, blue: 0 / 255, alpha: 1)
+        view.addSubview(label)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func ajouterAlveole(valeurX: CGFloat, valeurY: CGFloat, image: UIImage) {
+        let height: CGFloat = 70
+        let width: CGFloat = 100
+        let x = view.frame.width - (width * valeurX)
+        let y = view.frame.height - (height * valeurY)
+        let frame = CGRect(x: x, y: y, width: width, height: height)
+        let iv = UIImageView(frame: frame)
+        iv.image = image
+        iv.contentMode = .scaleAspectFit
+        
+        view.addSubview(iv)
+        alveoles.append(iv)
+        
     }
-
 
 }
 
